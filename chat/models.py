72cad6621db7324dbae0d051ffafe0e9f7fc2fed
has12zen <has12@iitbbs.ac.in>
2023-03-26 21:35:21 +0530

    Game Room Manager

--- chat/models.py
+++ chat/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+import secrets
+
 
 # Create your models here.
 
@@ -9,14 +13,107 @@ class User(models.Model):
     wins = models.IntegerField(default=0)
     losses = models.IntegerField(default=0)
 
-class Room(models.Model):
-    roomkey = models.CharField(max_length=255, unique=True)
-    userIds = models.CharField(max_length=255)
-    status = models.CharField(max_length=255)
-    winner = models.CharField(max_length=255)
-    PointsA = models.IntegerField(default=0)
-    PointsB = models.IntegerField(default=0)
-    PointsC = models.IntegerField(default=0)
-    PointsD = models.IntegerField(default=0)
-    Moves = models.CharField(max_length=255)
-    Turn = models.CharField(max_length=255)
+
+
+
+def generate_room_id():
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    room_id = ''.join(secrets.choice(alphabet) for i in range(5))
+    return room_id
+
+
+class GameRoomManager(models.Manager):
+    def create_room(self, player_one):
+        room = self.model(player_one=player_one, status='ACTIVE')
+        room.room_id = generate_room_id()
+        room.save()
+        return room
+
+    def join_room(self, room_id, player_id):
+        room = self.get(room_id=room_id)
+        if room.status == 'INACTIVE':
+            raise ValidationError('This room is already inactive')
+        if player_id in [room.player_one, room.player_two, room.player_three, room.player_four]:
+            return room;
+        if room.player_four:
+            raise ValidationError('This room is already full')
+        if not room.player_two:
+            room.player_two = player_id
+        elif not room.player_three:
+            room.player_three = player_id
+        elif not room.player_four:
+            room.player_four = player_id
+        room.save()
+        return room
+
+    def leave_room(self, room_id, player_id):
+        room = self.get(room_id=room_id)
+        if player_id == room.player_one:
+            room.delete()
+        elif player_id == room.player_two:
+            room.player_two = None
+        elif player_id == room.player_three:
+            room.player_three = None
+        elif player_id == room.player_four:
+            room.player_four = None
+        room.save()
+        return room
+
+    def make_move(self, room_id, player_id, move):
+        room = self.get(room_id=room_id)
+        if room.status == 'INACTIVE':
+            raise ValidationError('This room is already inactive')
+        if player_id != room.turn:
+            raise ValidationError('It is not your turn')
+        room.moves += f'{move},'
+        if room.turn == room.player_one:
+            room.turn = room.player_two
+        elif room.turn == room.player_two:
+            room.turn = room.player_three
+        elif room.turn == room.player_three:
+            room.turn = room.player_four
+        elif room.turn == room.player_four:
+            room.turn = room.player_one
+        room.save()
+        return room
+
+    def check_player_disconnection(self, room_id, player_id):
+        room = self.get(room_id=room_id)
+        if player_id == room.turn:
+            if player_id == room.player_one:
+                room.turn = room.player_two
+            elif player_id == room.player_two:
+                room.turn = room.player_three
+            elif player_id == room.player_three:
+                room.turn = room.player_four
+            elif player_id == room.player_four:
+                room.turn = room.player_one
+        room.save()
+        return room
+
+    def check_room_timeout(self, room_id):
+        room = self.get(room_id=room_id)
+        created_at = room.created_at
+        now = timezone.now()
+        time_difference = now - created_at
+        if time_difference.seconds >= 300:
+            room.status = 'INACTIVE'
+            room.save()
+        return room
+
+class GameRoom(models.Model):
+    objects = GameRoomManager()
+    ROOM_STATUS = (
+        ('ACTIVE', 'Active'),
+        ('INACTIVE', 'Inactive'),
+    )
+    room_id = models.CharField(max_length=10, unique=True)
+    player_one = models.CharField(max_length=100)
+    player_two = models.CharField(max_length=100, blank=True, null=True)
+    player_three = models.CharField(max_length=100, blank=True, null=True)
+    player_four = models.CharField(max_length=100, blank=True, null=True)
+    moves = models.TextField(blank=True, null=True)
+    turn = models.CharField(max_length=100, blank=True, null=True)
+    status = models.CharField(
+        max_length=10, choices=ROOM_STATUS, default='ACTIVE')
+    created_at = models.DateTimeField(auto_now_add=True)
--- chat/views.py
+++ chat/views.py
@@ -1,77 +1,52 @@
-from django.shortcuts import render , redirect
-from django.urls import reverse_lazy
+from django.forms import ValidationError
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView
-from chat.forms import SignUpForm
 from django.contrib.auth.forms import UserCreationForm
-from .models import User,Room
+from .models import User, GameRoom, GameRoomManager
+
 
 def createRoom(request):
-    roomkey = request.POST.get('roomkey')
-    
-    # validate form data
-    if not roomkey:
-        return render(request, 'chat/home.html', {'error': 'Room key is required.'})
-    
-    # check if room with roomkey already exists
-    if Room.objects.filter(roomkey=roomkey).exists():
-        return render(request, 'chat/home.html', {'error': 'Room with that key already exists.'})
-    
-    # create new room
-    room = Room.objects.create(
-        roomkey=roomkey,
-        userIds='',
-        status='active',
-        winner='',
-        PointsA=0,
-        PointsB=0,
-        PointsC=0,
-        PointsD=0,
-        Moves='',
-        Turn=''
-    )
-    
-    return redirect(f'/chat/{roomkey}') 
+    room = GameRoom.objects.create_room(request.user.username)
+
+    return redirect(f'/chat/{room.room_id}')
+
 
 def joinRoom(request):
     roomkey = request.POST.get('roomkey')
     user = request.user
     try:
-        room = Room.objects.get(roomkey=roomkey)
-    except Room.DoesNotExist:
-        return render(request, 'chat/home.html', {'error': 'Room with that key does not exist.'})
-    if not room.userIds:
-        room.userIds = str(user.id)
-    else:
-        user_ids = room.userIds.split(',')
-        if len(user_ids) >= 4:
-            return render(request, 'chat/home.html', {'error': 'Room is full.'})
-        user_ids.append(str(user.id))
-        room.userIds = ','.join(user_ids)
-    room.save()
-    
-    return redirect('/chat/' + roomkey) 
+        room = GameRoom.objects.join_room(roomkey, user.id)
+    except ValidationError as e:
+        return render(request, 'chat/home.html', {'error': e})
 
-def chatPage(request , *args , **kwargs) :
-    if not request.user.is_authenticated : 
+    room.save()
+
+    return redirect('/chat/' + roomkey)
+
+
+def chatPage(request, *args, **kwargs):
+    if not request.user.is_authenticated:
         return redirect("login-user")
     context = {}
     if request.method == 'POST':
         action = request.POST.get('action')
-        if(action == 'create'):
+        if (action == 'create'):
             return createRoom(request)
-        elif(action == 'join'):
+        elif (action == 'join'):
             return joinRoom(request)
-        else :
-            return render(request, 'chat/home.html',{'error': 'Invalid action. select create or join'})
+        else:
+            return render(request, 'chat/home.html', {'error': 'Invalid action. select create or join'})
     else:
         return render(request, 'chat/home.html')
 
+
 def room(request, room_name):
-    if not request.user.is_authenticated : 
+    if not request.user.is_authenticated:
         return redirect("login-user")
     return render(request, "chat/room.html", {"room_name": room_name})
 
 # Sign Up View
+
 
 def create_user(request):
     if request.method == 'POST':
@@ -84,7 +59,8 @@ def create_user(request):
                 wins=0,
                 losses=0
             )
-            return redirect('login-user') # replace 'home' with the name of your home view
+            # replace 'home' with the name of your home view
+            return redirect('login-user')
     else:
         form = UserCreationForm()
     return render(request, 'chat/SignUpPage.html', {'form': form})
